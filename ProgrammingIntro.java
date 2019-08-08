a57f303b0d212a01ec4b90ec1924623ea4658084
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-08 15:37:01 -0700

    Hi

---
+++ ProgrammingIntro.java
@@ -0,0 +1,23 @@
+public class ProgrammingIntro {
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    int result = myFunction(5, 10);
+    System.out.println(result);
+
+    // int thisIsAVariable =10;
+    // double decimalNumber = 13.95;
+    // boolean trueOrFalce = true;
+    // char aLetter = 'E';
+    // String someText = "EUReKA!";
+
+    // System.out.println(thisIsAVariable);
+    // System.out.println(decimalNumber);
+    // System.out.println(trueOrFalce);
+    // System.out.println(aLetter);
+    // System.out.println(someText);
+}
+public static int myFunction(int numberOne, int numberTwo){   
+    return numberOne* numberTwo;
+}
+}
